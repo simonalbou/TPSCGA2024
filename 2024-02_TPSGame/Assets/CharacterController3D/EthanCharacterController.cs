0610db6090a91131e586048575b1e43bade5e14f
Simon Albou <albou.simon@gmail.com>
2024-02-19 17:01:22 +0100

    TPS Character Controller (without Cinemachine)

--- 2024-02_TPSGame/Assets/CharacterController3D/EthanCharacterController.cs
+++ 2024-02_TPSGame/Assets/CharacterController3D/EthanCharacterController.cs
@@ -6,24 +6,30 @@ public class EthanCharacterController : MonoBehaviour
 {
     public float speed = 7;
     public float rotateSpeedRatio = 1;
+    public float camYawSpeed, camPitchSpeed;
+    public bool invertYaw, invertPitch;
     public CharacterController ctrl;
     public Animator animator;
-    public Transform self;
+    public Transform graphicsTransform;
+    public Transform camPivotYaw, camPivotPitch;
+    public Transform camTransform;
 
     Vector3 lastKnownLookAtDirection;
     Quaternion idealLookAtDirection;
 
     void Update()
     {
-        Vector3 move = Vector3.zero;
-
-        move.x = Input.GetAxis("Horizontal");
-        move.z = Input.GetAxis("Vertical");
+        // convertir "move" en espace caméra
+        Vector3 move = Input.GetAxis("Horizontal") * camTransform.right + Input.GetAxis("Vertical") * camTransform.forward;
 
         LookTowards(move);
 
         ctrl.Move(move * speed * Time.deltaTime);
         animator.SetFloat("Speed", move.magnitude);
+
+        // camera rotation
+        camPivotYaw.Rotate(Vector3.up * camYawSpeed * Time.deltaTime * Input.GetAxis("Horizontal_Cam") * (invertYaw ? -1 : 1));
+        camPivotPitch.Rotate(Vector3.right * camPitchSpeed * Time.deltaTime * Input.GetAxis("Vertical_Cam") * (invertPitch ? -1 : 1));
     }
 
     void LookTowards(Vector3 move)
@@ -34,6 +40,6 @@ public class EthanCharacterController : MonoBehaviour
             idealLookAtDirection = Quaternion.LookRotation(lastKnownLookAtDirection);
         }
         
-        self.rotation = Quaternion.Slerp(self.rotation, idealLookAtDirection, rotateSpeedRatio * Time.deltaTime);
+        graphicsTransform.rotation = Quaternion.Slerp(graphicsTransform.rotation, idealLookAtDirection, rotateSpeedRatio * Time.deltaTime);
     }
 }
--- 2024-02_TPSGame/Assets/Editor/EditorStartUpdateStructure.cs
+++ 2024-02_TPSGame/Assets/Editor/EditorStartUpdateStructure.cs
@@ -21,6 +21,7 @@ public class EditorStartUpdateStructure
     {
         if (EditorApplication.timeSinceStartup > timestamp + 5)
         {
+            //EditorStyles.label.normal.textColor = Color.green;
             //Debug.Log($"Unity has been up since {EditorApplication.timeSinceStartup} seconds.");
             //timestamp = EditorApplication.timeSinceStartup;
         }
